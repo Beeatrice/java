3e5da91873def9768783b9ed27eeb362d88cc90b
Beatriz Sanabria <105684798+Beeatrice@users.noreply.github.com>
2026-02-25 22:23:32 -0300

    Add Main class with variable declarations and output

---
+++ variaveis.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        String nome = "João";
+        int idade = 20;
+        double altura = 1.75;
+        
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+    }
+}
