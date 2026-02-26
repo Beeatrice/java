3038e7be84d804f4038355139e7dffec5fcc4f3d
Beatriz Sanabria <105684798+Beeatrice@users.noreply.github.com>
2026-02-25 22:24:21 -0300

    Update idade from 23 to 22

--- variaveis.java
+++ variaveis.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         
         String nome = "Thabia";
-        int idade = 23;
+        int idade = 22;
         double altura = 1.56;
         
         System.out.println("Nome: " + nome);
